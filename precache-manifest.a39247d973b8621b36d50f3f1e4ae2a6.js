fd5e2e6c861be77868145bed3df2d7938c8ada6b
Aman <styloamnald@gmail.com>
2021-01-12 20:42:42 +0530

    Updates

---
+++ precache-manifest.a39247d973b8621b36d50f3f1e4ae2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadb2afe4874c82d1a52fa36d60f013c",
+    "url": "/Backend_oxford/index.html"
+  },
+  {
+    "revision": "63d63f0f4f5888c32c1c",
+    "url": "/Backend_oxford/static/css/main.ab0e0662.chunk.css"
+  },
+  {
+    "revision": "5feaa36f19bbb19750f2",
+    "url": "/Backend_oxford/static/js/2.2803bc75.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Backend_oxford/static/js/2.2803bc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d63f0f4f5888c32c1c",
+    "url": "/Backend_oxford/static/js/main.61f0f27e.chunk.js"
+  },
+  {
+    "revision": "3d0abcd2a18e568c694a",
+    "url": "/Backend_oxford/static/js/runtime-main.846fe271.js"
+  }
+]);

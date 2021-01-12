1308aed02354d4aa822045c1390452cc60bc3a5e
AgrahariAman <31672142+AgrahariAman@users.noreply.github.com>
2021-01-12 19:49:41 +0530

    front end

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, WordList } from "./Components";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Navbar />
+			<WordList />
+		</div>
+	);
+}
+
+export default App;
